52006729040d8f0eb5ad03538958ed8868f125ef
GeekyD1no <21achahn@bishopkelley.org>
2018-07-10 15:53:24 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,13 +1,16 @@
 # Here's an example of a 'snacks' method that returns the meal choice passed in to it and defaults to "cheetos" if nothing is passed in.
 def snacks(food="Cheetos")
-  puts "Any time, is the right time for #{food}!"
+  "Any time, is the right time for #{food}!"
 end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on the readme (frosted flake, grilled cheese, salmon)
 
-def breakfast(food="frosted flakes")
- puts "Morning, is the right time for #{food}"
+def breakfast(breakfast="frosted flakes")
+  "Morning, is the right time for #{breakfast}"
 end
+
+puts breakfast
+
 
 
 
